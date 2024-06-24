8fa0340f9ca3116a9ef0bc563194f0b5c25e74ab
Benjamin <benjminrichie@gmail.com>
2024-06-24 14:20:27 +0000

    task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,18 +1,3 @@
-// export default function createIteratorObject(report) {
-//   let allEmployees = [];
-//   for (const item of Object.values(report.allEmployees)) {
-//     allEmployees = [
-//       ...allEmployees,
-//       ...item,
-//     ];
-//   }
-//   return allEmployees;
-// }
-
-
-
-
-
 export default function createIteratorObject(report) {
   const myarray = [];
   for (const item of Object.values(report.allEmployees)) {
